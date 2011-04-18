88054fe5e72f660dfbcecf0dd499da9b4405c4da
Anthony Perritano <aperritano@gmail.com>
2011-04-18 14:52:01 +0200

    Initial Commit

---
+++ iObserveTests/iObserveTests.h
@@ -0,0 +1,17 @@
+//
+//  iObserveTests.h
+//  iObserveTests
+//
+//  Created by Anthony John Perritano on 4/18/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface iObserveTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ iObserveTests/iObserveTests.m
@@ -0,0 +1,33 @@
+//
+//  iObserveTests.m
+//  iObserveTests
+//
+//  Created by Anthony John Perritano on 4/18/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "iObserveTests.h"
+
+
+@implementation iObserveTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in iObserveTests");
+}
+
+@end
